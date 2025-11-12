40b4cf2593401e5eebdfba430cf0bdd1e83452b2
JP-7808 <jayp7808@gmail.com>
2025-11-12 16:31:42 +0530

    base Url updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:6600/api' || 'https://vehicle-rental-backend-cctz.onrender.com/api' ;
+const API_BASE_URL = 'https://vehicle-rental-backend-cctz.onrender.com/api' || 'http://localhost:6600/api';
 
 // Create axios instance
 const api = axios.create({
