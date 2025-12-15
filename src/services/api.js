2b1897ddce2251692d032b8bb5f445f40af36ae1
JP-7808 <jayp7808@gmail.com>
2025-12-15 12:08:49 +0530

    Base url updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'https://api.asiaandcompanyselfdrive.com/api';
+const API_BASE_URL = 'https://vehicle-rental-backend-nydh2.ondigitalocean.app/api';
 // const API_BASE_URL = 'http://localhost:6600/api';
 
 // Create axios instance
